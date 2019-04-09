2300d39c7aec040b5c9273b8eaf15e2e49a45394
HyeonJaaE <presentlee914@gmail.com>
2019-04-09 15:52:12 +0900

    dp

--- swExpertAcademy/5642.py
+++ swExpertAcademy/5642.py
@@ -1,20 +1,25 @@
 T = int(input())
 for i in range(0,T):
     N = int(input())
-    a = list(map(int, input().split()))
+    li = list(map(int, input().split()))
+    dp_li = [0]
+    dp_li_tmp = [0]
+
     print("#", end='')
     print(i+1, end=' ')
 
-    max = a[0]
+    max = li[0]
 
     for j in range(0, N):
-        tmp = a[j]
-        if max < tmp :
-            max =tmp
+        dp_li_tmp[0] = li[j]
+        if dp_li_tmp[0] > max:
+            max = dp_li_tmp[0]
 
-        for t in range(j, N):
-            if t+1 < N :
-                tmp += a[t+1]
-            if max < tmp :
-                max = tmp
+        for k in range(1, j+1):
+            dp_li_tmp.append = dp_li_tmp[0] + dp_li[k-1]
+            if dp_li_tmp[k] > max:
+                max = dp_li_tmp[k]
+
+        dp_li = dp_li_tmp
+
     print(max)
