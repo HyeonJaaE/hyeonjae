216f322ecba48be56eccc53c2d6c2340ae6232fb
HyeonJaaE <presentlee914@gmail.com>
2019-04-11 19:30:45 +0900

    5642 pass
    DP 사용

--- swExpertAcademy/5642.py
+++ swExpertAcademy/5642.py
@@ -2,24 +2,21 @@ T = int(input())
 for i in range(0,T):
     N = int(input())
     li = list(map(int, input().split()))
-    dp_li = [0]
-    dp_li_tmp = [0]
+    dp = [1000000 for i in range(1000001)]
 
     print("#", end='')
     print(i+1, end=' ')
 
-    max = li[0]
+    dp[0] = li[0]
+    max = dp[0]
 
-    for j in range(0, N):
-        dp_li_tmp[0] = li[j]
-        if dp_li_tmp[0] > max:
-            max = dp_li_tmp[0]
-
-        for k in range(1, j+1):
-            dp_li_tmp.append = dp_li_tmp[0] + dp_li[k-1]
-            if dp_li_tmp[k] > max:
-                max = dp_li_tmp[k]
-
-        dp_li = dp_li_tmp
-
+    for j in range(1, N):
+        if dp[j-1] < 0:
+            dp[j] = li[j]
+            if dp[j] > max:
+                max = dp[j]
+        else:
+            dp[j] = dp[j-1] + li[j]
+            if dp[j] > max:
+                max = dp[j]
     print(max)
