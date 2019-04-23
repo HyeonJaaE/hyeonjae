eed44a72bb82562be6cea54e421ab5d939f08d20
HyeonJaaE <presentlee914@gmail.com>
2019-04-23 17:53:18 +0900

    Update K번째수.cpp
    heap stl  사용
    성공

--- programmers/K번째수.cpp
+++ programmers/K번째수.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,21 +8,17 @@ vector<int> solution(vector<int> array, vector<vector<int>> commands) {
     vector<int> answer;
     vector<int> v;
 
-    for(int k = 0 ; k < commands.size() ; k++){
-        for(int i = commands[k][0]-1 ; i < commands[k][1]; i++ ){
-            if(v.size() == 0){
-                v.push_back(array[i]);
-            }
-            else{
-                for(int j = 0 ; j < v.size(); j++){
-                    if(array[i] < v[j]){
-                        v.insert(v.begin() + j, array[i]);
-                    }
-                }
-            }
+    while(!commands.empty()){
+        for(int i = commands[0][0] - 1 ; i < commands[0][1] ; i++){
+            v.push_back(array[i]);
         }
+        make_heap(v.begin(), v.end());
+        sort_heap(v.begin(), v.end());
+
+
+        answer.push_back(v[commands[0][2] -1]);
+        commands.erase(commands.begin());
         v.clear();
-        answer.push_back(v[commands[k][2]-1]);
     }
     return answer;
 }
