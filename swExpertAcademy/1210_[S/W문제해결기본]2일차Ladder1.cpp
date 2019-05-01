97962bb4b3d7ed2b3f3c2ddd0ef6f1b0454ad199
HyeonJaaE <presentlee914@gmail.com>
2019-05-01 14:21:49 +0900

    Create W문제해결기본]2일차Ladder1.cpp
    파일입출력용

---
+++ swExpertAcademy/1210_[S/W문제해결기본]2일차Ladder1.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<string.h>
+#include<fstream>
+using namespace std;
+
+int arr[100][100];
+
+int search(int r, int c ) {
+
+	while (1) {
+		/*
+		1000010001
+		1111110001
+		1000010001
+		1000011111
+		1000010002
+		*/
+		if (c - 1 >= 0 && arr[r][c - 1] == 1) {
+			int tmp = c - 1;
+			while (arr[r][tmp] == 1 && tmp >= 0) {
+				tmp--;
+			}
+			c = tmp + 1;
+			r++;
+		}
+		else if (c + 1 < 100 && arr[r][c + 1] == 1) {
+			int tmp = c + 1;
+			while (arr[r][tmp] == 1 && tmp < 100) {
+				tmp++;
+			}
+			c = tmp - 1;
+			r++;
+		}
+		else {
+			r++;
+		}
+
+
+		if (arr[r][c] == 2) {
+			return 1;
+		}
+		else if (r == 99) {
+			return 0;
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+
+	int T = 10;
+	int Tcnt = 0;
+
+	ifstream fin;
+	fin.open("input.txt");
+	ofstream fout("output.txt");
+
+	while (T--) {
+
+		fin >> Tcnt;
+		int ans = 0;
+		memset(arr, 0, sizeof(arr));
+
+		for (int r = 0; r < 100; r++) {
+			for (int c = 0; c < 100; c++) {
+				fin >> arr[r][c];
+			}
+		}
+
+		for (int i = 0; i < 100; i++) {
+			int tmp = 0;
+			if(arr[0][i] == 1) tmp = search(0, i);
+			if (tmp == 1) {
+				ans = i;
+				break;
+			}
+		}
+
+		fout << "#" << Tcnt << " " << ans << endl;
+	}
+
+	fin.close();
+	fout.close();
+
+	return 0;
+}
